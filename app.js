0a4d1ad73a546cfb3174e3228263ec2742749d77
Arbab khan <ak2077719@gmail.com>
2022-12-23 00:00:16 +0530

    added port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const products_routes = require("./routes/products")
 const connectDb = require("./db/connec")
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const URI = process.env.URI;
 app.get("/",(req,res)=>{
     res.send("Hello server!");
